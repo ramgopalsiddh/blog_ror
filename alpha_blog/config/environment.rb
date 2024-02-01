b288fcdeffc0d2f10d0ffecfff5984c1f76efec6
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-02-01 20:13:53 +0530

    Day 8 Style validation and flash messages

--- alpha_blog/config/environment.rb
+++ alpha_blog/config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Stop Rails default styleing for preventing clash between Rails default style and bootstrap
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
