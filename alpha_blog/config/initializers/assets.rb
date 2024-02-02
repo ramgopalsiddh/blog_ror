a76443075945c920be3a4a1dd3440d667bafb993
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-02-02 12:56:44 +0530

    Day 9 solve don't Access custom.css errer

--- alpha_blog/config/initializers/assets.rb
+++ alpha_blog/config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = "1.0"
 
 Rails.application.config.assets.precompile += %w(bootstrap.min.js popper.js)
+Rails.application.config.assets.precompile += %w(bootstrap.min.css)
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
