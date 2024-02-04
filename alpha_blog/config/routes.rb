5fbe9ca5e47138bb2d770d400531f179cc15387a
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-02-04 20:23:17 +0530

    Day 11 Add new user form, routes and controller

--- alpha_blog/config/routes.rb
+++ alpha_blog/config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles#, only: [:show, :index, :new, :create, :edit, :update, :destroy] # comented section use for specific route 
+  resources :articles#, only: [:show, :index, :new, :create, :edit, :update, :destroy] # comented section use for specific route
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
