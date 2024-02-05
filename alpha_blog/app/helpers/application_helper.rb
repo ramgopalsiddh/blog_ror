6aa2c9b16827378c3f13eba8bf647dea478778a1
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-02-05 14:31:37 +0530

    Day 12 cleanup layout, improve experience and view

--- alpha_blog/app/helpers/application_helper.rb
+++ alpha_blog/app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 
-  def avatar_for(user, options = { size: '150x150' })
+  def avatar_for(user, options = { size: '80x80' })
     email_address = user.email.downcase
     hash = Digest::MD5.hexdigest(email_address)
     size = options[:size]
     robot_url = "https://robohash.org/#{hash}.png/bgset_any?size=#{size}"
-    image_tag(robot_url, alt: user.username, class: "rounded-circle shadow mx-auto d-block")
+    image_tag(robot_url, alt: user.username, class: "rounded-3 shadow mx-auto d-block")
   end
 
 end
