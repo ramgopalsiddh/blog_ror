eefc4995fda101e9d9cd7c16df98d9a5e8cadff0
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-02-07 00:08:45 +0530

    Day 14 Restrict actions for articles controller

--- alpha_blog/app/controllers/articles_controller.rb
+++ alpha_blog/app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   # this remove code retendency
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy] 
 
   def show
     #binding.break # In Rails 6 this is byebug this use for debug
@@ -55,6 +57,13 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:alert] = "You can only edit or delete your own article"
+      redirect_to @article
+    end
   end
 
 
