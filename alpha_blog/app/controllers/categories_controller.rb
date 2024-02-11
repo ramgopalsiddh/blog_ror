a4475f614806e424854b29ba73a24fbfd56a1807
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-02-12 01:51:18 +0530

    Day 19 Add Edit categories option for Admin

--- alpha_blog/app/controllers/categories_controller.rb
+++ alpha_blog/app/controllers/categories_controller.rb
@@ -15,9 +15,22 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name updated sucessfully"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
   def index
     @pagy, @categories = pagy(Category.all, items: 5)
-
   end
 
   def show
